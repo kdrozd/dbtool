fc86ba579f21e2e4be782a8ee41d98af95fc41d3
Krzysztof Drozd <drozd.krzysiek@gmail.com>
2021-08-08 13:03:47 +0200

    Cleaning and file managment

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# dbtool
+# DBTool
+
+Command line tool to do some "ad-hoc" stuff with data in your database.
+
+List of supported commands (functionalities) can be seen below.
+
+Requirements:
+- Java 11 (JRE)
+- Command line execution environment (Bash, Powershel,...)
+
+# Implemented commands
+
+# Roadmap
+
+# `select`
+# `insert`
+# `delete`
+# `update`
+# `extract`
+# `search`
+
+# `reformat`
+## `reformat insert`
+## `reformat select`
+## `reformat dates`
+
+
--- src/main/java/nl/drozd/tools/db/dbtool/MainCommand.java
+++ src/main/java/nl/drozd/tools/db/dbtool/MainCommand.java
@@ -3,8 +3,15 @@ package nl.drozd.tools.db.dbtool;
 public class MainCommand {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// extract: extract schema conntent (think about CBLOB columns as they may be very big
+		// select : execute SQL Select (generic version)
+		// delete : execute SQL Delete
+		// insert : -||- Insert
+		// update : -||- Update
+		// search : use patter matching for column matching and search for things in values
 
 	}
+
+
 
 }
